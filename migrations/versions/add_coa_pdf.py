62447812880279ed86f7c8ae817673d8f7ef0cd9
roasted-garlic <85726502+roasted-garlic@users.noreply.github.com>
2024-12-18 19:00:39 +0000

    Restored to 'bfac0fb15f728acab3b953dd4165adcb5f3127f2'
    Replit-Restored-To:bfac0fb15f728acab3b953dd4165adcb5f3127f2

--- migrations/versions/add_coa_pdf.py
+++
@@ -1,9 +0,0 @@
-
-from alembic import op
-import sqlalchemy as sa
-
-def upgrade():
-    op.add_column('product', sa.Column('coa_pdf', sa.String(500)))
-
-def downgrade():
-    op.drop_column('product', 'coa_pdf')
